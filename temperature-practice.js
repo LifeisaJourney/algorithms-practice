eb209b395b82217648d1f5ba6c72949d09d8ddea
LifeH <hwie.zhu@gmail.com>
2018-11-26 20:52:43 -0500

    started on newton calculation

--- temperature-practice.js
+++ temperature-practice.js
@@ -1,4 +1,16 @@
-let kelvin = 293; 
-let celsius = kelvin - 273;
+// 293K 
+let kelvin = 0;
 
-let fahrenheit = celsius * (9/5) + 32;
+// new celcius from kelvin
+let celsius = kelvin -273;
+let newton = celsius * (33/100)
+//calculating fahrenheit
+let fahrenheit = celsius * (9/5) + 32
+// Math.floor(variable)
+fahrenheit= Math.floor(fahrenheit);
+
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+console.log(`The temperature is ${newton} degrees Fahrenheit.`);
+
+newton = Math.floor(newton);
